09f8001143ed06b521844a9d60530cbd3ee8d1a4
Kyle Cordell <kcordell-git@outlook.com>
2023-07-15 16:40:59 -0700

    Adds solution for challenge 3-4

--- javascript/code-challenges/challenges-03.test.js
+++ javascript/code-challenges/challenges-03.test.js
@@ -42,9 +42,7 @@ Write a function named oddValues that, given an array of integers as input, uses
 For example, oddValues([1,2,3]) returns [1,3].
 ------------------------------------------------------------------------------------------------ */
 
-const oddValues = (arr) => {
-  // Solution code here...
-};
+const oddValues = (arr) => arr.filter(integer => integer % 2 > 0);
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 5
