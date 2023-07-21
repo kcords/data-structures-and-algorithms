81e5d05bf7b8bf777c175218d15c7e937548baa2
Kyle Cordell <kcordell-git@outlook.com>
2023-07-20 19:49:43 -0700

    Adds solution for challenge 4-5

--- javascript/code-challenges/challenges-04.test.js
+++ javascript/code-challenges/challenges-04.test.js
@@ -76,7 +76,7 @@ For example, ['Alphabet', 'Zebra', 'alphabet', 'carrot'] is correctly sorted.
 ------------------------------------------------------------------------------------------------ */
 
 const alphabetize = (arr) => {
-  // Solution code here...
+  return arr.sort((a, b) => a.charCodeAt(0) - b.charCodeAt(0));
 };
 
 /* ------------------------------------------------------------------------------------------------
