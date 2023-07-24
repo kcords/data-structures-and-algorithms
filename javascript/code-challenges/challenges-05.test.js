eda95d188b3735a814b59d874eba248c91bc0302
Kyle Cordell <kcordell-git@outlook.com>
2023-07-23 19:22:52 -0700

    Adds solution for challenge 5-2

--- javascript/code-challenges/challenges-05.test.js
+++ javascript/code-challenges/challenges-05.test.js
@@ -23,7 +23,7 @@ Write a function named addValues that, given an array of numbers as input, uses 
 ------------------------------------------------------------------------------------------------ */
 
 const addValues = (arr) => {
-  // Solution code here...
+  return arr.reduce((sum, val) => sum + val, 0);
 };
 
 /* ------------------------------------------------------------------------------------------------
