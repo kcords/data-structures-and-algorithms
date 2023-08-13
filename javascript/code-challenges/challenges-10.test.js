c6c78b625a53ccdd4393ed6516c066b84ab0f888
Kyle Cordell <kcordell-git@outlook.com>
2023-08-12 21:23:12 -0700

    Adds challenge 11

--- javascript/code-challenges/challenges-10.test.js
+++ javascript/code-challenges/challenges-10.test.js
@@ -192,12 +192,22 @@ const averageDailyTemperature = (weather) =>
   ) /
   (weather.length * 7);
 
-let lowestWeeklyTemperatureData = [
-  [33, 64, 58, 65, 71, 57, 60],
-  [40, 45, 33, 53, 44, 59, 48],
-  [55, 54, 60, 53, 59, 57, 61],
-  [65, 56, 55, 52, 55, 62, 57],
-];
+  /* ------------------------------------------------------------------------------------------------
+  CHALLENGE 10 - Stretch Goal
+
+  Write a function named lowestWeeklyAverage that accepts a two-dimensional array of daily temperatures grouped week-by-week.
+
+  Calculate the average temperature for each week and return the value of the lowest weekly average temperature.
+
+  For example, in the data set below, the lowest weekly average is 46, which is the average of the temperatures in week 2. All other weeks have average temperatures that are greater than 46.
+  ------------------------------------------------------------------------------------------------ */
+
+  let lowestWeeklyTemperatureData = [
+    [33, 64, 58, 65, 71, 57, 60],
+    [40, 45, 33, 53, 44, 59, 48],
+    [55, 54, 60, 53, 59, 57, 61],
+    [65, 56, 55, 52, 55, 62, 57],
+  ];
 
 const lowestWeeklyAverage = (weather) =>
   weather.reduce(
