9bae06af4376323e112d419e45f825b24d607e7b
Kyle Cordell <70975298+kcords@users.noreply.github.com>
2023-10-16 21:11:35 -0700

    Adds includes method to LinkedList

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -20,6 +20,14 @@ class LinkedList:
         new_node = Node(value, self.head)
         self.head = new_node
 
+    def includes(self, value):
+        current = self.head
+        while current is not None:
+            if current.value == value:
+                return True
+            current = current.next
+        return False
+
 
 class TargetError:
     pass
