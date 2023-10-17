32e171a3dfd7745095a2e5c666deac9be213ab1c
Kyle Cordell <70975298+kcords@users.noreply.github.com>
2023-10-16 20:57:14 -0700

    Adds Node class to linked_list.py

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -1,7 +1,17 @@
+class Node:
+    def __init__(self, value, next=None):
+        """Initializes the creation of a new Node
+
+        Args:
+            value (any): Value of the node (typically an int).
+            next (Node, optional): Pointer to the next node. Defaults to None.
+        """
+        self.value = value
+        self.next = next
+
+
 class LinkedList:
-    """
-    Put docstring here
-    """
+
 
     def __init__(self):
         # initialization here
