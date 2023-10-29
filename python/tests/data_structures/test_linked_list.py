4bb295068aaf60dcec9487743581ebb33ee9b6a8
Kyle Cordell <70975298+kcords@users.noreply.github.com>
2023-10-29 11:54:45 -0700

    Adds insertion tests to LinkedList

--- python/tests/data_structures/test_linked_list.py
+++ python/tests/data_structures/test_linked_list.py
@@ -1,5 +1,5 @@
 import pytest
-from data_structures.linked_list import LinkedList
+from data_structures.linked_list import LinkedList, Node, TargetError
 
 
 def test_exists():
@@ -65,3 +65,56 @@ def test_includes_false():
     linked_list.insert("banana")
 
     assert not linked_list.includes("cucumber")
+
+
+def test_append_existing_list(default_linked_list):
+    new_item = "cherry"
+    default_linked_list.append(new_item)
+    assert default_linked_list.includes(new_item)
+    assert str(default_linked_list) == f"{{ apple }} -> {{ banana }} -> {{ pear }} -> {{ {new_item} }} -> NULL"
+
+
+def test_append_empty_list():
+    linked_list = LinkedList()
+    new_item = "new_item"
+    linked_list.append(new_item)
+    assert linked_list.includes(new_item)
+    assert str(linked_list) == f"{{ {new_item} }} -> NULL"
+
+
+def test_insert_before_valid(default_linked_list):
+    new_item = "cherry"
+    default_linked_list.insert_before("banana", new_item)
+    assert default_linked_list.includes(new_item)
+    assert str(default_linked_list) == f"{{ apple }} -> {{ {new_item} }} -> {{ banana }} -> {{ pear }} -> NULL"
+
+
+def test_insert_before_invalid(default_linked_list):
+    new_item = "new item"
+    invalid_item = "invalid item"
+    with pytest.raises(TargetError):
+        default_linked_list.insert_before(invalid_item, new_item)
+
+
+def test_insert_after_valid(default_linked_list):
+    new_item = "cherry"
+    default_linked_list.insert_after("banana", new_item)
+    assert default_linked_list.includes(new_item)
+    assert str(default_linked_list) == f"{{ apple }} -> {{ banana }} -> {{ {new_item} }} -> {{ pear }} -> NULL"
+
+
+def test_insert_after_invalid(default_linked_list):
+    new_item = "new item"
+    invalid_item = "invalid item"
+    with pytest.raises(TargetError):
+        default_linked_list.insert_after(invalid_item, new_item)
+
+
+@pytest.fixture
+def default_linked_list():
+    linked_list = LinkedList()
+    linked_list.head = Node("apple")
+    linked_list.head.next = Node("banana")
+    linked_list.head.next.next = Node("pear")
+    return linked_list
+
